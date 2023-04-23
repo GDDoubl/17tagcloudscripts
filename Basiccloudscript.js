a0557db038fc4f4526932f434145844c203af145
17 <101656277+GDDoubl@users.noreply.github.com>
2023-04-23 12:32:42 -0500

    Update and rename Cloudscriptfuni.js to Basiccloudscript.js

---
+++ Basiccloudscript.js
@@ -0,0 +1,3 @@
+handlers.ReturnCurrentVersion = function(args){
+    return {"ResultCode":0,"BannedUsers":"banned users","MOTD":"message of the day","SynchTime":"speakertime", "Message":"version"}
+}
