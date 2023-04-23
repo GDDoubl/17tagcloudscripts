d558de904690703b8df845bb0626f12e3c5e8361
17 <101656277+GDDoubl@users.noreply.github.com>
2023-04-23 12:32:56 -0500

    Delete Basiccloudscript.js

--- Basiccloudscript.js
+++
@@ -1,3 +0,0 @@
-handlers.ReturnCurrentVersion = function(args){
-    return {"ResultCode":0,"BannedUsers":"banned users","MOTD":"message of the day","SynchTime":"speakertime", "Message":"version"}
-}
